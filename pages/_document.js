8ce0408873d5aca9cbbb3bf67bc762ad7ed44cc8
ChuongTranKT <vanchuongktt1998@gmail.com>
2022-05-13 22:32:23 +0700

    setup theme ui

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,13 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;700&family=Montserrat:wght@100;400;700&display=swap"
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
         </Head>
---
+++ untils/theme.js
@@ -0,0 +1,83 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#574CE6",
+      dark: "#575CE6",
+      light: "#575CE9",
+    },
+    secondary: {
+      main: "#77CDFE",
+      dark: " #C193F5",
+      light: "#FFE345",
+    },
+    error: {
+      main: "#d32f2f",
+      light: "#ef5350",
+      dark: "#c62828",
+      contrastText: "#fff",
+    },
+    warning: {
+      main: "#ed6c02",
+      light: "#ff9800",
+      dark: "#e65100",
+      contrastText: "#fff",
+    },
+    info: {
+      main: "#0288d1",
+      light: "#03a9f4",
+      dark: "#01579b",
+      contrastText: "#fff",
+    },
+    success: {
+      main: "#2e7d32",
+      light: "#4caf50",
+      dark: "#1b5e20",
+      contrastText: "#fff",
+    },
+  },
+  typography: {
+    fontFamily: "Inter",
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+    fontSize: 14,
+    h1: {
+      fontFamily: "Inter",
+      fontWeight: 700,
+      fontSize: "3.4rem",
+    },
+    h2: {
+      fontFamily: "Inter",
+      fontWeight: 700,
+      fontSize: "2.4rem",
+    },
+    h3: {
+      fontFamily: "Inter",
+      fontWeight: 500,
+      fontSize: "2.0rem",
+    },
+    h4: {
+      fontFamily: "Inter",
+      fontWeight: 500,
+      fontSize: "1.6rem",
+    },
+    h5: {
+      fontFamily: "Inter",
+      fontWeight: 500,
+      fontSize: "1.5rem",
+    },
+    h6: {
+      fontFamily: "Inter",
+      fontWeight: 300,
+      fontSize: "1.3rem",
+    },
+    button: {
+      fontFamily: "Inter",
+      fontWeight: 500,
+      fontSize: "1rem",
+    },
+  },
+});
