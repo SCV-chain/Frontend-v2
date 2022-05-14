65300af37062ce4606576924c99d8d26ae779e9a
Sony Trinh <trunghieu912003@gmail.com>
2022-05-14 22:15:54 +0700

    added Errorboundary

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,25 @@
-import { CacheProvider } from "@emotion/react";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import createEmotionCache from "utils/createEmotionCache";
-import "@styles/globals.css";
-import theme from "@styles/theme";
-import Head from "next/head";
-import PropTypes from "prop-types";
-import { StoreProvider } from "@lib/zustandProvider";
-import { useHydrate } from "@lib/store";
+import { CacheProvider } from '@emotion/react'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import createEmotionCache from 'utils/createEmotionCache'
+import '@styles/globals.css'
+import theme from '@styles/theme'
+import Head from 'next/head'
+import PropTypes from 'prop-types'
+import { StoreProvider } from '@lib/zustandProvider'
+import { useHydrate } from '@lib/store'
+import ErrorBoundary from '@components/common/ErrorBoundary'
 
-const Noop = ({ children }) => <>{children}</>;
+const Noop = ({ children }) => <>{children}</>
 // Client-side cache shared for the whole session
 // of the user in the browser.
 
-const clientSideEmotionCache = createEmotionCache();
+const clientSideEmotionCache = createEmotionCache()
 
-function MyApp({
-  Component,
-  pageProps,
-  emotionCache = clientSideEmotionCache,
-}) {
-  const store = useHydrate(pageProps.initialZustandState);
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }) {
+  const store = useHydrate(pageProps.initialZustandState)
 
-  const Layout = Component.Layout ?? Noop;
+  const Layout = Component.Layout ?? Noop
   return (
     <CacheProvider value={emotionCache}>
       <Head>
@@ -41,18 +38,20 @@ function MyApp({
         <CssBaseline />
         <Layout>
           <StoreProvider store={store}>
-            <Component {...pageProps} />
+            <ErrorBoundary>
+              <Component {...pageProps} />
+            </ErrorBoundary>
           </StoreProvider>
         </Layout>
       </ThemeProvider>
     </CacheProvider>
-  );
+  )
 }
 
 MyApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
   emotionCache: PropTypes.object,
   pageProps: PropTypes.object.isRequired,
-};
+}
 
-export default MyApp;
+export default MyApp
